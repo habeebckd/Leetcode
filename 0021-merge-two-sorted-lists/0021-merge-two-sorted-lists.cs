9956356b91a7ef751c828cf00705ca9bf871c37f
habeebckd <habee0483@gmail.com>
2025-05-01 20:59:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        int[] newarr={ , };
+    for(i=0;i<legth.list1;i++){
+        for(j=i+1;j<legth.list2;j++){
+            if(list1==list2 || list1>list2){
+                newarr++;
+            }else{
+                newarr++;
+            }
+        }
+    }
+        
+    }
+     return newarr[];
+}
