f078441532c1036d7bada182bb0cef70a015d3a9
habeebckd <habee0483@gmail.com>
2025-05-10 14:54:54 +0530

    Time: 0 ms (100%), Space: 40.1 MB (12.41%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cs
+++ 0058-length-of-last-word/0058-length-of-last-word.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int LengthOfLastWord(string s) {
-        s=s.Trim();
-        string b = s.Split(" ").Last();
+        // s = s.Trim() if use here reduse twise use trim
+        string b = s.Trim().Split(" ").Last().Trim();
         return b.Length;
             }
 }
